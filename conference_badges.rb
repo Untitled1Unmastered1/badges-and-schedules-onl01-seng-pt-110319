2cdddbe227607658cd815a9c6918be62167692ba
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-13 02:50:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,6 @@ def assign_rooms(room_assignments)
   room_assignments.each_with_index do |room_assignments, index|
     puts "Hello, #{room_assignments}! You'll be assigned to room _____!" 
   end
-  return empty_rooms
-end 
     
 
   
