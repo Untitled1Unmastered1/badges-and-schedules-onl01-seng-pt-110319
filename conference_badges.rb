de27dcd0f845a89a3dcc064ae386dd8d00797148
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-13 03:18:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(room_assignments)
   empty_rooms = []
   room_assignments.each_with_index do |room_assignments, index|
-    empty_rooms <<  "Hello, #{room_assignments}! You'll be assigned to room #{1}!"
+    empty_rooms << puts "Hello, #{room_assignments}! You'll be assigned to room #{1}!"
   end
   empty_rooms
 end 
